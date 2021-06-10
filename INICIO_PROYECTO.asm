4a0785b63bd03b89132b4d108586b8f03ad87787
Darien Márquez <darien1005@hotmail.com>
2021-06-10 08:43:11 -0600

    update2

--- INICIO_PROYECTO.asm
+++ INICIO_PROYECTO.asm
@@ -1,12 +1,41 @@
-.model
+;/**********************************************************************************\
+;|			4 PAOLA MONSERRAT DIAZ BRISENO
+;|			8 DARIEN RAFEL MARQUEZ VAZQUEZ
+;|			Hora: 11:00 a.m.
+;\**********************************************************************************/
+;Inclusión de Librerias de Macros
+include Utilerias.LIB
+include Motor_Solar.LIB
+.model small
 .stack
 .data 
 
     nombre    db 16,0,16 dup ('$')
     msjN      db 'Ingresa tu nombre: $'
     comprueba db 10,13,'Bienvenido a Blue Ametyst: $'
+    msjContinuar    db  'Presione cualquier tecla para continuar...'
+    ruta            db 'C:\4_8_ProyectoU3\',0
+    ruta1           db 'C:\4_8_ProyectoU3\BlueAM',0
+    ruta2           db 'C:\4_8_ProyectoU3\BlueAM\Reporte.txt',0
+     
+    msjIni          db 'Creando directorio...',10,13,'$'
+    msjSi           db 'Se ha creado el directorio',10,13,'$'
+    msjNO           db 'No se ha creado el directorio',10,13,'$'
+     
+    msjSiEscribir   db 'Se ha escrito en el archivo. Verifique$'
+    msjNoEscribir   db 'No se ha podido ESCRIBIR en el archivo. Verifique$'
+      
+    msjNOLeer       db 'NO se ha podido leer el archivo'
+    msjNOAbrir      db 'NO se ha podido abrir el archivo'
     
 
+    msjSalida       db 10,13, 'Datos le',161,'dos del archivo:',10,13
+    leido           db 500 dup('$')    
+
+    ;--DATOS DE ESCRITURA--
+    datosBlueAm      db 'Movimiento Registrado, verificar estado del panel!' ;Datos a escribir en el archivo
+    totalEscribir   dw  45
+    manejador       dw  0  
 ;* l o g o-- *********************************************************************
                                                                                        
 logo   db 'B#KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKNBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB'
@@ -46,17 +75,6 @@ logo27 db 'hbBBBBBBBB%YPBB9gBBBBo]BBBBQ]]QBBgjYYttttt%BBBBBCeBBBBBBBZu#BBBBBBPtp
     proceso         db  'Ventana de menu'
 ;*****************************************************
 ;                   MACROS                            
-CADENA_COLOR MACRO cadena,long,ren,col,pag,modo,color
-        MOV AH,19
-        LEA BP,cadena   ;Cadena
-        MOV CX,long     ;longitud de cadena
-        MOV DH,ren      ;Renglon
-        MOV DL,col      ;columna
-        MOV BH,pag      ;pagina
-        MOV AL,modo     ;modo
-        MOV BL,color    ;Atributo (Color)
-        INT 10H
-CADENA COLOR ENDM
 
 ;*****************************************FIN MACROS**    
 .code
@@ -128,6 +146,131 @@ inicio:
                         MOV AH,9
                         LEA DX, nombre+2 ;+2 por los byte de control
                         INT 21H
+		    ;ACTIVAR PAG2
+	    #start=stepper_motor.exe# 
+	    ;---
+        pasos = 02h ; 32 (decimal)       
+datcw   db 0000_0011b
+        db 0000_0110b
+        db 0000_1100b
+        db 0000_1001b
+        
+        MOV CX,pasos           ;inicializa el contador con el numero de pasos
+MOTOR_INICIO: MOV BX, offset datcw ;inicializa el apuntado
+        MOV SI,0             ;inicializa el contador
+SIGUIENTE_PASO:
+WAIT:   IN AL,07H            ;obtiene la informacion del puerto 07H
+        TEST AL,10000000b
+        JZ WAIT
+        MOV AL, [BX][SI]
+        OUT 7, AL
+        INC SI
+        CMP SI, 4
+        JC SIGUIENTE_PASO
+        MOV SI,0
+        DEC CL
+        JZ EscrituraReporte1
+        JMP SIGUIENTE_PASO
+EscrituraReporte1: ;escribir a archivo
+	    ;---
+	    ;MOTOR_CONTROL 15H
+	    CALL TECLA
+	        MOV AH,5
+	        MOV AL,2
+	        INT 10H
+	    CADENA_COLOR        proceso,    40, 0,  40,    1,  0,  2
+
+        ;--CREAR ARCHIVOS EN DIRECTORIO ALMACENADO EN LA VARIABLE ruta1
+        
+        MOV AH,39H
+        LEA DX,ruta
+        INT 21H 
+        
+        MOV AH,39H
+        LEA DX,ruta1
+        INT 21H
+        
+        jc errorCrear
+        
+        CADENA_SIN_COLOR msjSi
+
+        ;--ESCRIBIR ARCHIVOS TXT 
+        
+        ;1. Abrir el archivo
+            MOV AH,3DH    ;ABRIR ARCHIVO
+            LEA DX,ruta2
+            MOV AL,2  ;
+            INT 21H
+            MOV manejador,AX ;Recuperar ID
+                
+            JC errorAbrir
+             
+            ; Se pudo abrir: 
+                
+                MOV AH,40H          ;Escribir en el archivo
+                MOV BX,manejador    
+                MOV CX,totalEscribir 
+                LEA DX,datosBlueAm  ; Escritura de datos generados por el sistema!
+                INT 21H
+                
+            JC errorEscribir
+            
+                CADENA_SIN_COLOR msjSiEscribir
+                CADENA_SIN_COLOR msjContinuar
+                CALL TECLA
+                CALL CLEAN_SCREEN
+        ;--LECTURA DE ARCHIVOS TXT 
+        
+        ;1. Abrir el archivo
+        mov ah,3dh
+        lea dx,ruta2
+        mov al,2  ;lectura/escritura
+        int 21h
+        
+        mov manejador,ax 
+        jc errorAbrir  
+        
+        ;2. Proceder a la lectura
+        mov ah,3fh
+        mov cx,15 ;caracteres a leer
+        lea dx,leido
+        mov bx,manejador    
+        int 21h
+        jc errorLeer 
+        
+        ;3. Mostrar datos en pantalla
+        
+        CADENA_SIN_COLOR msjSalida
+        CADENA_SIN_COLOR leido
+        CADENA_SIN_COLOR msjContinuar
+        CALL TECLA
+        CALL CLEAN_SCREEN
+        jmp fin
+
+errorCrear: 
+    CADENA_SIN_COLOR msjNo
+    CADENA_SIN_COLOR msjContinuar
+    CALL TECLA
+    CALL CLEAN_SCREEN        
+errorAbrir: 
+    CADENA_SIN_COLOR msjNOabrir
+    jmp fin
+    CADENA_SIN_COLOR msjContinuar
+    CALL TECLA
+    CALL CLEAN_SCREEN
+    
+errorEscribir:
+
+    CADENA_SIN_COLOR msjNOEscribir      
+    CADENA_SIN_COLOR msjContinuar
+    CALL TECLA
+    CALL CLEAN_SCREEN
+errorLeer: 
+    CADENA_SIN_COLOR msjNOleer
+    CADENA_SIN_COLOR msjContinuar
+    CALL TECLA
+    CALL CLEAN_SCREEN
+	
 fin:
     MOV AX,4C00H
     INT 21H 
